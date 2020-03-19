c257f603318ab3090e93f7f5e048af9f286cf3fe
Ryan Russell <ryan@ryanrussell.ca>
2020-03-19 11:00:44 -0400

    Output basic Rusher struct to view.

---
+++ lib/nfl_rushing_web/structs/rusher.ex
@@ -0,0 +1,17 @@
+defmodule NflRushingWeb.Rusher do
+  defstruct player: "",
+            team: "",
+            pos: "",
+            att: 0,
+            att_game: 0,
+            yds: 0,
+            avg: 0,
+            yds_game: 0,
+            td: 0,
+            lng: 0,
+            first: 0,
+            first_percent: 0,
+            twenty_plus: 0,
+            forty_plus: 0,
+            fum: 0
+end
